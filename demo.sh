84223886de616e84e7b435756b42701703150810
lmhtq <lmhtq1991@gmail.com>
2014-08-22 13:26:55 +0800

    test

---
+++ demo.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+#the root folder of vlc's source code
+#and the path variable must end with '/'
+
+VLC_ROOT=~/codes/vlc-2.1.4/vlc-2.1.4/
+PWD=$(cd "$(dirname "$0")"; pwd)
+codec_dir=${VLC_ROOT}modules/codec
+rtp_dir=${VLC_ROOT}modules/access/rtp
+
+if [ ! -f $codec_dir/x264.c.bak ]; then  
+mv $codec_dir/x264.c $codec_dir/x264.c.bak
+fi
+
+if [ ! -f $rtp_dir/session.c.bak ]; then  
+mv $rtp_dir/session.c $rtp_dir/session.c.bak
+fi
+
+if [ -L $codec_dir/x264.c ]; then  
+rm $codec_dir/x264.c
+ln -s $PWD/x264.c $codec_dir/x264.c
+else
+ln -s $PWD/x264.c $codec_dir/x264.c
+fi
+
+if [ -L $rtp_dir/session.c ]; then  
+rm $rtp_dir/session.c
+ln -s $PWD/session.c $rtp_dir/session.c
+else
+ln -s $PWD/session.c $rtp_dir/session.c
+fi
+
+if [ -L $codec_dir/user_udp.h ]; then  
+rm $codec_dir/user_udp.h $codec_dir/user_udp.c
+ln -s $PWD/user_udp.h $codec_dir/user_udp.h
+ln -s $PWD/user_udp.c $codec_dir/user_udp.c
+else
+ln -s $PWD/user_udp.h $codec_dir/user_udp.h
+ln -s $PWD/user_udp.c $codec_dir/user_udp.c
+fi
+
+if [ -L $rtp_dir/user_udp.h ]; then  
+rm $rtp_dir/user_udp.h $rtp_dir/user_udp.c
+ln -s $PWD/user_udp.h $rtp_dir/user_udp.h
+ln -s $PWD/user_udp.c $rtp_dir/user_udp.c
+else
+ln -s $PWD/user_udp.h $rtp_dir/user_udp.h
+ln -s $PWD/user_udp.c $rtp_dir/user_udp.c
+fi
+
